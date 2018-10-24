e50f85c56047ad2fd5642d279dd050401a9bb48d
Wagner Nogueira <wagner@ephrom.com.br>
2018-10-24 15:55:51 -0300

    criando tela de chat

---
+++ flutter_app/lib/ChatScreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: true,
+      top: true,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat App"),
+          centerTitle: true,
+          elevation: Theme.of(context).platform == TargetPlatform.iOS? 0.0 : 4.0
+        ),
+        body: Column(
+
+        ),
+      ),
+    );
+  }
+}
